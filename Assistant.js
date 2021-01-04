b2ae2050ac03ba8823dff1e8ca414c0927bc1617
Barthdry <67647112+Barthdry@users.noreply.github.com>
2021-01-04 12:58:04 +0400

    Add files via upload

--- Assistant.js
+++ Assistant.js
@@ -1,5 +1,13 @@
 alert("Hi I am your personal assistant");
 var myAssistantName = prompt("What would you like to call me");
 document.getElementById("footer").innerHTML = myAssistantName + " Personal Assistant <BR> Barthdry Corporation 2019 - 2021";
-prompt("Ask Me Anything");
+var Gsearch = prompt("Ask Me Anything");
+function googlesearch(ans)
+{
+    query = ans;
+    url ='http://www.google.com/search?q=' + query;
+    window.open(url,'_blank');
+}
+document.getElementById("result").innerHTML = "Allow Popups to Perform Search Functions"
+googlesearch(Gsearch)
 
