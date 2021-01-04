6a0ec8a4a188b695b15dfd9ebd2b530d03f21829
Barthdry <67647112+Barthdry@users.noreply.github.com>
2021-01-04 11:35:54 +0400

    Add files via upload

---
+++ Assistant.js
@@ -0,0 +1,5 @@
+alert("Hi I am your personal assistant");
+var myAssistantName = prompt("What would you like to call me");
+document.getElementById("footer").innerHTML = myAssistantName + " Personal Assistant <BR> Barthdry Corporation 2019 - 2021";
+prompt("Ask Me Anything");
+
