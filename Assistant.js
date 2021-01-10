edfdfde50721c675cc80fa0fef225b3fa7ec87dd
Barthdry <67647112+Barthdry@users.noreply.github.com>
2021-01-10 11:25:08 +0400

    Made Clearer Code

--- Assistant.js
+++ Assistant.js
@@ -1,8 +1,12 @@
 alert("Hi I am your personal assistant");
 var myAssistantName = prompt("What would you like to call me");
+if (myAssistantName == ""){
+document.getElementById("footer").innerHTML = "JS Personal Assistant <BR> Barthdry Corporation 2019 - 2021";
+} else{
 document.getElementById("footer").innerHTML = myAssistantName + " Personal Assistant <BR> Barthdry Corporation 2019 - 2021";
-var askans = prompt("Ask Me anything");
-if ( askans = "Google"){
+}
+switch(prompt("Ask Me anything")){
+case "Google":
 var Gsearch = prompt(" Google Anything");
 function googlesearch(ans){ 
     query = ans;
@@ -11,11 +15,11 @@ function googlesearch(ans){
 }
 document.getElementById("result").innerHTML = "Allow Popups to Perform Search Functions";
 googlesearch(Gsearch);
-} else if (ans = "Add") {
+case "add":
 var num1 = Prompt("Enter Number 1");
 var num2 = Prompt("Enter Number 2");
 var sum = num1 + num2 
 Alert("The Sum is" + sum)
-} else {
+ default: 
 document.getElementById("result").innerHTML = "Sorry I don't Understand";
 }
